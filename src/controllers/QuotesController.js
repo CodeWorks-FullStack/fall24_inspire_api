cdad466a9a79227d3c11c65b24149f072306ad41
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-12 10:37:04 -0600

    created quote

--- src/controllers/QuotesController.js
+++ src/controllers/QuotesController.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import { quotesService } from "../services/QuotesService.js";
 import BaseController from "../utils/BaseController.js";
 
@@ -6,12 +7,29 @@ export class QuotesController extends BaseController {
     super('api/quotes')
     this.router
       .get('', this.getRandomQuote)
+      // NOTE anything below (after) this use means you have to be logged in (have a valid bearer token)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.createQuote)
   }
 
   async getRandomQuote(request, response, next) {
     try {
       const randomQuote = await quotesService.getRandomQuote()
       response.send(randomQuote)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createQuote(request, response, next) {
+    try {
+      const quoteData = request.body
+      // NOTE the user making the request
+      const user = request.userInfo
+      // NOTE this will overwrite the authorId if it is present in the body, or create the property if it is not present
+      quoteData.authorId = user.id
+      const quote = await quotesService.createQuote(quoteData)
+      response.send(quote)
     } catch (error) {
       next(error)
     }
--- src/services/QuotesService.js
+++ src/services/QuotesService.js
@@ -1,11 +1,18 @@
 import { dbContext } from "../db/DbContext.js"
 
 class QuotesService {
+
   async getRandomQuote() {
     // const quote = await dbContext.Quotes.findOne().populate('author', '-email -subs')
     const quotesCount = await dbContext.Quotes.countDocuments()
     const randomSkipAmount = Math.floor(Math.random() * quotesCount)
     const quote = await dbContext.Quotes.findOne().skip(randomSkipAmount).populate('author', 'name picture')
+    return quote
+  }
+
+  async createQuote(quoteData) {
+    const quote = await dbContext.Quotes.create(quoteData)
+    await quote.populate('author', 'name picture')
     return quote
   }
 }
