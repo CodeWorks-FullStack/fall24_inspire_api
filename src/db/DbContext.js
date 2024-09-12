4eb23e019b1af8551ed9e4079c57c7d530da6b69
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-12 10:43:31 -0600

    registered schema

--- src/db/DbContext.js
+++ src/db/DbContext.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { QuoteSchema } from '../models/Quote.js';
+import { ImageSchema } from '../models/Image.js';
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
   Quotes = mongoose.model('Quote', QuoteSchema);
+  Images = mongoose.model('Image', ImageSchema)
 
 }
 
