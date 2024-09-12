2ea01becdc66268dd602b10da0c5481cc65639eb
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-12 10:06:35 -0600

    getting random quote

--- src/models/Quote.js
+++ src/models/Quote.js
@@ -10,3 +10,10 @@ export const QuoteSchema = new Schema(
     toJSON: { virtuals: true }
   }
 )
+
+QuoteSchema.virtual('author', {
+  localField: 'authorId',
+  ref: 'Account',
+  foreignField: '_id',
+  justOne: true
+})
