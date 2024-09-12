5e28b61788721766f3966bfdd3ad178e0b4c0aeb
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-12 11:06:23 -0600

    create image

--- src/services/ImagesService.js
+++ src/services/ImagesService.js
@@ -1,11 +1,18 @@
 import { dbContext } from "../db/DbContext.js"
 
 class ImagesService {
+
   async getRandomImage() {
     const imagesCount = await dbContext.Images.countDocuments()
     const randomSkipAmount = Math.floor(Math.random() * imagesCount)
     const randomImage = await dbContext.Images.findOne().populate('author', 'name picture').skip(randomSkipAmount)
     return randomImage
+  }
+
+  async createImage(imageData) {
+    const image = await dbContext.Images.create(imageData)
+    await image.populate('author', 'name picture')
+    return image
   }
 
 }
